bb5948daf9badb1ea9de26e13b7a06fc75bdc64e
Dmitry Kozyrev <Ko3bIpb1625347@gmail.com>
2020-05-05 12:30:23 +0900

    Decorator review has done

--- Decorator/main.cpp
+++ Decorator/main.cpp
@@ -4,12 +4,15 @@
 using std::string;
 using std::cout;
 
+/* The component's basic interface defines behavior that is changed by decorators. */
 class Component {
 public:
     virtual ~Component() = default;
     [[nodiscard]] virtual string Operation() const = 0;
 };
 
+/* Concrete Components provide default behavior implementations.
+ * There may be several variations of these classes. */
 class ConcreteComponent : public Component {
 public:
     [[nodiscard]] string Operation() const override {
@@ -17,16 +20,21 @@ public:
     }
 };
 
+/* The Decorator base class follows the same interface as the other components.
+ * The main goal of this class is to define a wrapper interface for all specific decorators. */
 class Decorator : public Component {
 protected:
     Component *component_;
 public:
     explicit Decorator(Component *component) : component_(component) {};
+    /* The decorator delegates all the work to the wrapped component. */
     [[nodiscard]] string Operation() const override {
         return this->component_->Operation();
     }
 };
 
+/* Concrete Decorators call a wrapped object
+ * and modify its result in some way. */
 class ConcreteDecoratorA : public Decorator {
 public:
     explicit ConcreteDecoratorA(Component* component) : Decorator(component) {}
@@ -37,14 +45,14 @@ public:
 
 class ConcreteDecoratorB : public Decorator {
 public:
-    explicit ConcreteDecoratorB(Component* component) : Decorator(component) {
-    }
+    explicit ConcreteDecoratorB(Component* component) : Decorator(component) {}
 
     [[nodiscard]] string Operation() const override {
         return "ConcreteDecoratorB(" + Decorator::Operation() + ")";
     }
 };
 
+/* Client code works with all objects using the Component interface. */
 void ClientCode(Component* component) {
     cout << "RESULT: " << component->Operation();
 }

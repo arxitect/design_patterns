924edb23d63e79a10cdba4f08bbe5969e5f5fe08
Dmitry Kozyrev <Ko3bIpb1625347@gmail.com>
2020-05-07 13:56:02 +0900

    Facade review has done

--- Facade/main.cpp
+++ Facade/main.cpp
@@ -4,6 +4,10 @@
 using std::cout;
 using std::string;
 
+/* The subsystem can accept requests either from the
+ * facade or from the client directly.
+ * In any case, for the Subsystem, the Facade is another client,
+ * and it is not part of the Subsystem.*/
 class Subsystem1 {
 public:
     [[nodiscard]] string Operation1() const {
@@ -24,6 +28,11 @@ public:
     }
 };
 
+/* The Facade class provides a simple interface
+ * for the complex logic of one or more subsystems.
+ * The facade delegates client requests to appropriate
+ * objects within the subsystem. The facade is also
+ * responsible for managing their life cycle. */
 class Facade {
 protected:
     Subsystem1 *subsystem1_;
@@ -48,11 +57,16 @@ public:
     }
 };
 
+/* Client code works with complex subsystems
+ * through a simple interface provided by the Facade. */
 void ClientCode(Facade *facade)
 {
     cout << facade->Operation();
 }
 
+/* In the client code, some subsystem objects may already be created.
+ * In this case, it may be appropriate to initialize the Facade with
+ * these objects instead of allowing the Facade to create new instances. */
 int main()
 {
     Subsystem1 *subsystem1 = new Subsystem1;

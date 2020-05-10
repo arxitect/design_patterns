e0add22c8c4e1abb862da949d32400d62fcf36dc
Dmitry Kozyrev <Ko3bIpb1625347@gmail.com>
2020-05-10 20:22:59 +0900

    Flyweight review has done

--- Flyweight/main.cpp
+++ Flyweight/main.cpp
@@ -12,6 +12,9 @@ using std::initializer_list;
 using std::make_pair;
 using std::pair;
 
+/* Purpose: Allows you to accommodate a larger number of objects in the allocated RAM.
+ * Flyweight saves memory by sharing the general state of objects among themselves,
+ * instead of storing the same data in each object. */
 struct SharedState
 {
     string brand_;
@@ -39,6 +42,9 @@ struct UniqueState
     }
 };
 
+/* Flyweight stores a common part of the state (also called internal state)
+ * that belongs to several real business objects. The Flyweight accepts the
+ * remainder of the state (an external state unique to each object) through its method parameters. */
 class Flyweight
 {
 private:
@@ -49,6 +55,7 @@ public:
     ~Flyweight() {
         delete sharedState_;
     };
+
     [[nodiscard]] SharedState *sharedState() const {
         return sharedState_;
     }
@@ -58,11 +65,16 @@ public:
     }
 };
 
+/* The FlyweightFactory creates and manages Lightweight objects.
+ * It provides the correct separation of flyweights. When a client
+ * requests a flyweight, the factory either returns an existing
+ * instance or creates a new one if it does not already exist. */
 class FlyweightFactory
 {
 private:
     unordered_map<string , Flyweight> flyweights;
 
+    /* Returns the hash of the Flyweight string for the given state. */
     string GetKey(const SharedState &sharedState) const {
         return sharedState.brand_ + "_" + sharedState.model_ + "_" + sharedState.color_;
     }
@@ -74,7 +86,7 @@ public:
                                         (this->GetKey(sharedState), Flyweight(&sharedState)));
         }
     }
-
+    /* Returns an existing Flyweight with the specified state or creates a new one. */
     Flyweight GetFlyweight(const SharedState &sharedState){
         string key = this->GetKey(sharedState);
         if(this->flyweights.find(key) == this->flyweights.end()) {
@@ -106,7 +118,14 @@ void addCarToPoliceDatabase(FlyweightFactory &ff, const string &plates, const st
 
 int main()
 {
-    FlyweightFactory *factory = new FlyweightFactory({{"Chevrolet", "Camaro2018", "pink"}, {"Mercedes Benz", "C300", "black"}, {"Mercedes Benz", "C500", "red"}, {"BMW", "M5", "red"}, {"BMW", "X6", "white"}});
+    FlyweightFactory *factory = new FlyweightFactory({
+        {"Chevrolet", "Camaro2018", "pink"},
+        {"Mercedes Benz", "C300", "black"},
+        {"Mercedes Benz", "C500", "red"},
+        {"BMW", "M5", "red"},
+        {"BMW", "X6", "white"}
+    });
+
     factory->listFlyweights();
 
     addCarToPoliceDatabase(*factory,
@@ -122,6 +141,7 @@ int main()
                            "BMW",
                            "X1",
                            "red");
+
     factory->listFlyweights();
     delete factory;
     return 0;

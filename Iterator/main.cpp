50b86ddbe71535ad48f967135078127694312400
Dmitry Kozyrev <Ko3bIpb1625347@gmail.com>
2020-05-19 13:58:27 +0900

    Iterator class

--- Iterator/main.cpp
+++ Iterator/main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using std::string;
 using std::cout;
+using std::vector;
+
+template<typename T, typename U>
+class Iterator {
+public:
+    typedef typename vector<T>::iterator iter_type;
+    Iterator(U *p_data, bool reverse = false) : m_p_data_(p_data) {
+        m_it_ = m_p_data_->m_data_.begin();
+    }
+
+    void First() {
+        m_it_ = m_p_data_->m_data_.begin();
+    }
+
+    void Next() {
+        m_it_++;
+    }
+
+    bool IsDone() {
+        return (m_it_ == m_p_data_->m_data_.end());
+    }
+
+    iter_type Current() {
+        return m_it_;
+    }
+
+private:
+    U *m_p_data_;
+    iter_type m_it_;
+};
 
 int main()
 {

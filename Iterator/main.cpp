d2aad60e427520a4644463b20f6dfe4b7cb9d0af
Dmitry Kozyrev <Ko3bIpb1625347@gmail.com>
2020-05-20 21:30:33 +0900

    Iterator has done

--- Iterator/main.cpp
+++ Iterator/main.cpp
@@ -3,8 +3,9 @@
 #include <vector>
 
 using std::string;
-using std::cout;
 using std::vector;
+using std::cout;
+using std::endl;
 
 template<typename T, typename U>
 class Iterator {
@@ -42,7 +43,7 @@ private:
     vector<T> m_data_;
 public:
     void Add(T a) {
-        m_data_.push_back();
+        m_data_.push_back(a);
     };
     Iterator<T, Container> *createIterator() {
         return new Iterator<T, Container>(this);
@@ -54,9 +55,42 @@ private:
     int m_data_;
 public:
     Data(int a = 0) : m_data_(a) {}
+
+    void setData(int a) {
+        m_data_ = a;
+    }
+    int data() {
+        return m_data_;
+    }
 };
+
+void ClientCode()
+{
+    cout <<  "________________Iterator with int______________________________________" << endl;
+    Container<int> cont;
+
+    for(int i = 0;i < 10;i++)
+        cont.Add(i);
+
+    Iterator<int, Container<int>> *it = cont.createIterator();
+    for(it->First(); !it->IsDone(); it->Next())
+        cout << *it->Current() << endl;
+
+    Container<Data> cont2;
+    Data a(100), b(1000), c(10000);
+    cont2.Add(a);
+    cont2.Add(b);
+    cont2.Add(c);
+
+    cout << "________________Iterator with custom Class______________________________" << endl;
+
+    Iterator<Data, Container<Data>> *it2 = cont2.createIterator();
+    for (it2->First(); !it2->IsDone(); it2->Next())
+        cout << it2->Current()->data() << endl;
+}
 
 int main()
 {
+    ClientCode();
     return 0;
 }
